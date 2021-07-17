eb40572a897db96d77999668226ee18b2e474b92
Shubham Kochar <shubhamkochar23@gmail.com>
2021-07-17 15:33:49 +0530

    AddTwoNumbers.py file added

---
+++ AddTwoNumbers.py
@@ -0,0 +1,13 @@
+# Python code to add two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("\nFirst number: {}\nSecond number: {}" .format(num1,num2))
+
+# Adding two numbers
+sum = num1 + num2
+
+# Display the sum
+# will print the value in flaot
+print("Sum of {} and {} is {}" .format(num1,num2,sum))

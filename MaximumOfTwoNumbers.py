c805c1f252313da776682459f43e046fe9587995
Shubham Kochar <shubhamkochar23@gmail.com>
2021-07-17 15:48:12 +0530

    MaximumOfTwoNumbers.py python file added

---
+++ MaximumOfTwoNumbers.py
@@ -0,0 +1,10 @@
+# Python code to find the maximum number between two numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if (num1 >= num2):
+    print("\n{} is greater than {}" .format(num1,num2))
+else:
+    print("\n{} is greater than {}" .format(num2,num1))
+

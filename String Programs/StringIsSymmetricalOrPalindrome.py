30d8a1fe793f9242b2daf86534a164b4ac6913e3
Shubham Kochar <shubhamkochar23@gmail.com>
2021-07-29 09:49:31 +0530

    Code file modified

--- String Programs/StringIsSymmetricalOrPalindrome.py
+++ String Programs/StringIsSymmetricalOrPalindrome.py
@@ -2,17 +2,36 @@
 
 def symmetrical(s):
     size = len(s)
-    print("Length: ",size)
-    mid = int(size/2)
-    k = mid
-    for i in range(0,mid):
-        if s[i] == s[k]:
-            k +=1
-        else: 
-            print("False")
+    flag = 0
+    if size %2:
+        mid = size//2 +1
+    else:
+        mid = size//2
+    
+    start1 =0
+    start2 =mid 
+
+    while(start1 < mid and start2 < size):
+        if(s[start1] == s[start2]):
+            start1 +=1
+            start2 +=1
+        else:
+            flag = 1
             break
-        print("True")
-        break
 
+    if flag == 0:
+        print("Entered string is symmetrical")
+    else:
+        print("Entered string is not symmetrical")
 
-symmetrical("khokpo")
+def palindrome(s):
+    if s == s[::-1]:
+        print("Entered string is palindrome")
+    else:
+        print("Entered string is not plaindrome")
+
+symm = str(input("Enter string to check its symmetricity: "))
+palin = str(input("Enter string to check palindrome: "))
+
+symmetrical(symm)
+palindrome(palin)
